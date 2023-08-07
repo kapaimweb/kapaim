f1ca916f91c7dcf98010e37aabec7ac83a7aa1d2
Ziv200 <ziv200@gmail.com>
2023-08-07 14:15:52 +0300

    added videos section

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,12 +1,52 @@
 //css
 import classes from "./Videos.module.css";
-
+//swiper
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+//react-youtube
+import YouTube from "react-youtube"; // Import the react-youtube library
+//bootstrap
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+//videoList
+import videoList from "./videoList";
 
 const Videos = () => {
   return (
     <div className={classes.videos}>
-      <Container>mmgkm</Container>
+      <Container>
+        <Row>
+          <h2 className={classes.videos_h}>מה אנחנו מנגנים</h2>
+        </Row>
+        <Row>
+          <Col>
+            <Swiper grabCursor='true' spaceBetween={50} slidesPerView={3}>
+              {videoList.map((video) => (
+                <SwiperSlide key={video.id}>
+                  <Card className='text-center'>
+                    <Card.Header> {video.title} </Card.Header>
+                    <Card.Body>
+                      <YouTube
+                        videoId={video.id}
+                        className='youtube-player'
+                        opts={{
+                          width: "100%",
+                          playerVars: {
+                            autoplay: 0,
+                            controls: 0,
+                          },
+                        }}
+                      />
+                    </Card.Body>
+                  </Card>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
---
+++ src/components/Videos/videoList.js
@@ -0,0 +1,10 @@
+const videoList = [
+  { id: "C3rw7BO4FL8", title: "חאפלה ים תיכונית" },
+  { id: "lRJJvtaq05Q", title: "מוזיקה בלקנית" },
+  { id: "jNuwyspN3FQ", title: "מוזיקה  יוונית" },
+  { id: "5Vc3x773cfU", title: "כלייזמר צועני" },
+  { id: "WfPXD8Dj_Mo", title: "תהלוכות ומופעי רחוב" },
+  { id: "098", title: "ליווי חתן וכלה" },
+];
+
+export default videoList;
