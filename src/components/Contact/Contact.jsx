07859650149fb7cc5023c2a8a78de4bac6bfcf91
kapaimweb <kapaimweb@gmail.com>
2023-08-07 17:37:15 +0300

    netlify form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -28,25 +28,28 @@ const Contact = () => {
           <h2 className={classes.contact_h}>צרו קשר</h2>
         </Row>
         <Form onSubmit={handleSubmit} name='contact'>
+          {/*this is for netlify bot to detect the form */}
+          <input type='hidden' name='form-name' value='contact' />
+          {/*  */}
           <Row>
             <Col lg={4} xl={4}>
               <Form.Group className='mb-4'>
                 <Form.Label className={classes.heebo}>שם</Form.Label>
-                <Form.Control required type='text' />
+                <Form.Control name='name' required type='text' />
               </Form.Group>
               <Form.Group className='mb-4'>
                 <Form.Label className={classes.heebo}>טלפון</Form.Label>
-                <Form.Control required type='' />
+                <Form.Control name='phone' required type='' />
               </Form.Group>
               <Form.Group className='mb-4'>
                 <Form.Label className={classes.heebo}>אימייל</Form.Label>
-                <Form.Control required type='email' />
+                <Form.Control name='email' required type='email' />
               </Form.Group>
             </Col>
             <Col lg={6} xl={6}>
               <Form.Group className='mb-4'>
                 <Form.Label className={classes.heebo}>הודעה</Form.Label>
-                <Form.Control className='py-2' rows={9} required as='textarea' />
+                <Form.Control name='message' className='py-2' rows={9} required as='textarea' />
               </Form.Group>
             </Col>
             <Col className={classes.submit} lg={2} xl={2}>
