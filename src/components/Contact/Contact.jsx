02057e65c694d9206e548f7e1ac3b34aa218512e
Ziv200 <ziv200@gmail.com>
2023-08-07 15:53:59 +0300

    form fixed in contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,33 +12,35 @@ const Contact = () => {
         <Row>
           <h2 className={classes.contact_h}>צרו קשר</h2>
         </Row>
-        <Row>
-          <Col lg={4} xl={4}>
-            <Form.Group className='mb-4'>
-              <Form.Label className={classes.heebo}>שם</Form.Label>
-              <Form.Control required type='text' />
-            </Form.Group>
-            <Form.Group className='mb-4'>
-              <Form.Label className={classes.heebo}>טלפון</Form.Label>
-              <Form.Control required type='' />
-            </Form.Group>
-            <Form.Group className='mb-4'>
-              <Form.Label className={classes.heebo}>אימייל</Form.Label>
-              <Form.Control required type='email' />
-            </Form.Group>
-          </Col>
-          <Col lg={6} xl={6}>
-            <Form.Group className='mb-4'>
-              <Form.Label className={classes.heebo}>הודעה</Form.Label>
-              <Form.Control className='py-2' rows={9} required as='textarea' />
-            </Form.Group>
-          </Col>
-          <Col className={classes.submit} lg={2} xl={2}>
-            <Button className={classes.submitbtn} variant='secondary'>
-              שלח
-            </Button>
-          </Col>
-        </Row>
+        <Form>
+          <Row>
+            <Col lg={4} xl={4}>
+              <Form.Group className='mb-4'>
+                <Form.Label className={classes.heebo}>שם</Form.Label>
+                <Form.Control required type='text' />
+              </Form.Group>
+              <Form.Group className='mb-4'>
+                <Form.Label className={classes.heebo}>טלפון</Form.Label>
+                <Form.Control required type='' />
+              </Form.Group>
+              <Form.Group className='mb-4'>
+                <Form.Label className={classes.heebo}>אימייל</Form.Label>
+                <Form.Control required type='email' />
+              </Form.Group>
+            </Col>
+            <Col lg={6} xl={6}>
+              <Form.Group className='mb-4'>
+                <Form.Label className={classes.heebo}>הודעה</Form.Label>
+                <Form.Control className='py-2' rows={9} required as='textarea' />
+              </Form.Group>
+            </Col>
+            <Col className={classes.submit} lg={2} xl={2}>
+              <Button type='submit' className={classes.submitbtn} variant='secondary'>
+                שלח
+              </Button>
+            </Col>
+          </Row>
+        </Form>
       </Container>
     </div>
   );
