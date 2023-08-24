0dbda91d1e5f8f446c79e2b80a32dc9fe67861c4
kapaimweb <kapaimweb@gmail.com>
2023-08-24 17:16:17 +0300

    fixed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
-import Weddings from "./pages/weddings";
+import Weddings from "./pages/Weddings";
 import Parade from "./pages/Parade";
 import "./scrollstyle.css";
 function App() {
