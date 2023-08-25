8892e2348a7e618c01121e49470531dd19de90c3
kapaimweb <kapaimweb@gmail.com>
2023-08-25 19:45:35 +0300

    fixed hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,11 +9,13 @@ const Hero = () => {
       <div className={classes.content}>
         <img src={logo_text} alt='kapaim logo' className={classes.logo_img} />
         <p>גרוב ים-תיכוני בלקני</p>
-        <Button
-          href='https://wa.me/972555668759?text=שלום%20להקת%20כפיים%20,%20אני%20מעוניין%20לשאול%20שאלה'
-          className={classes.cta}>
-          דברו איתנו
-        </Button>
+        <button className={classes.cta}>
+          <a
+            className={classes.cta_link}
+            href='https://wa.me/972555668759?text=שלום%20להקת%20כפיים%20,%20אני%20מעוניין%20לשאול%20שאלה'>
+            דברו איתנו
+          </a>
+        </button>
       </div>
       <div className={classes.caret_wrap}>
         <div className={classes.caret}>
